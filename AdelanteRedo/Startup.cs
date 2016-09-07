aedbca8f6c5e037ee66009ccb6a5bef27c6c2fa3
JohnathonDouglas19 <jcdoug03@cardmail.louisville.edu>
2016-09-07 16:42:58 -0400

    Add project files.

---
+++ AdelanteRedo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdelanteRedo.Startup))]
+namespace AdelanteRedo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
