d140fe2ef37dd4e9ed4603ddb0e85dceb131022e
JohnathonDouglas19 <jcdoug03@cardmail.louisville.edu>
2016-09-14 17:09:05 -0400

    Fixed Mess ups

---
+++ AdelanteRedo/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdelanteRedo.Models
+{
+    public class Course
+    {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public int Credits { get; set; }
+
+            public virtual ICollection<Enrollment> Enrollments { get; set; }
+        }
+    }
---
+++ AdelanteRedo/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AdelanteRedo.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+    }
+}
