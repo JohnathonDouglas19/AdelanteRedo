3217a730223178087d5712a50a125305c4482ec8
Josh Woosley <josh.woosley@gmail.com>
2016-09-24 15:50:17 -0400

    Added Attendance and Parents controller with CRUD done for both

---
+++ AdelanteRedo/Controllers/ParentsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DataLib;
+
+namespace AdelanteRedo.Controllers
+{
+    public class ParentsController : Controller
+    {
+        private goyals420_troutEntities db = new goyals420_troutEntities();
+
+        // GET: Parents
+        public ActionResult Index()
+        {
+            return View(db.Parents.ToList());
+        }
+
+        // GET: Parents/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Parent parent = db.Parents.Find(id);
+            if (parent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(parent);
+        }
+
+        // GET: Parents/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Parents/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Parent_NUM,Parent_FirstName,Parent_LastName,Parent_Address,Parent_City,Parent_State,Parent_Zip,Parent_HomeTele,Parent_CellPhone,Parent_Email")] Parent parent)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Parents.Add(parent);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(parent);
+        }
+
+        // GET: Parents/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Parent parent = db.Parents.Find(id);
+            if (parent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(parent);
+        }
+
+        // POST: Parents/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Parent_NUM,Parent_FirstName,Parent_LastName,Parent_Address,Parent_City,Parent_State,Parent_Zip,Parent_HomeTele,Parent_CellPhone,Parent_Email")] Parent parent)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(parent).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(parent);
+        }
+
+        // GET: Parents/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Parent parent = db.Parents.Find(id);
+            if (parent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(parent);
+        }
+
+        // POST: Parents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Parent parent = db.Parents.Find(id);
+            db.Parents.Remove(parent);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
