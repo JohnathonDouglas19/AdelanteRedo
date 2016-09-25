5e47c841598d56865bce9af9f6a3e2f8cf1c84f2
Megan B <meg.balcom@gmail.com>
2016-09-25 11:15:23 -0400

    Added model, view, and controller for Program

---
+++ AdelanteRedo/Models/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdelanteRedo.Models
+{
+    public class Program
+    {
+        public int ProgramID {get; set;}
+
+        public string ProgramName { get; set; }
+
+    }
+}
