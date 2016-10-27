a8f6b9f18d4795b663e3799fcc471645dd7e350f
Megan B <meg.balcom@gmail.com>
2016-10-27 00:49:31 -0400

    added images to img folder for board officers and added parallax to 2 sections

--- AdelanteRedo/Scripts/_references.js
+++ AdelanteRedo/Scripts/_references.js
@@ -98,6 +98,7 @@
 /// <reference path="jquery.validate.min.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
 /// <reference path="jquery-1.10.2.js" />
+/// <reference path="jquery-1.12.4.js" />
 /// <reference path="jquery-3.1.1.js" />
 /// <reference path="jquery-3.1.1.min.js" />
 /// <reference path="jquery-3.1.1.slim.min.js" />
